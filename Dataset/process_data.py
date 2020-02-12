4a9a9e787af02eb6d8babb053fd786d31da1817f
ahsanayub <md.ahsanayub@outlook.com>
2020-02-11 20:58:18 -0600

    Update on Processing Data script

--- Dataset/process_data.py
+++ Dataset/process_data.py
@@ -9,103 +9,200 @@ __status__ = "Prototype"
 import os
 import glob
 import pandas as pd
+import numpy as np
 
 def createList(data, family_id, class_id):
+    
     temp_master_data = []
-    temp_master_data.append(data[1][0])             # Operation
-    temp_master_data.append(int(data[1][1], 16))    # Sequence Number
-    h, m, s, f = data[1][2].split(':')              # Pre operation time
+    
+    # Operation
+    data[1][0] = data[1][0].strip()
+    if (data[1][0] == "IRP" or data[1][0] == "irp"):
+        temp_master_data.append(1)
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+    
+    elif (data[1][0] == "FSF" or data[1][0] == "fsf"):
+        temp_master_data.append(0)
+        temp_master_data.append(1)
+        temp_master_data.append(0)
+        
+    elif (data[1][0] == "FIO" or data[1][0] == "fio"):
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+        temp_master_data.append(1)
+    
+    else:
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+        
+    temp_master_data.append(int((data[1][1].strip()), 16))              # Sequence Number
+    
+    data[1][2] = data[1][2].strip()
+    h, m, s, f = data[1][2].split(':')                                  # Pre operation time
     pre_operation_time = int(h) * 3600 + int(m) * 60 + int(s) + int(f) / 1000
     temp_master_data.append(pre_operation_time)
-    h, m, s, f = data[1][3].split(':')              # Pre operation time
+    
+    data[1][3] = data[1][3].strip()
+    h, m, s, f = data[1][3].split(':')                                  # Pre operation time
     post_operation_time = int(h) * 3600 + int(m) * 60 + int(s) + int(f) / 1000
     temp_master_data.append(post_operation_time)
+    
     temp_master_data.append(post_operation_time - pre_operation_time)   # operation_elapsed
-    process_id, thread_id = str(data[1][4]).split('.')  # dataset has process_id.thread_id as float
-    temp_master_data.append(int(process_id))    # process id
-    temp_master_data.append(int(thread_id))     # thread id
-    temp_master_data.append(int(data[1][5]))    # parent id
-    temp_master_data.append(data[1][6])         # process name
-    temp_master_data.append(data[1][7])         # major operation type
-    try:
-        data[1][8] = str(data[1][8]).split(' ')[0]      # IRP flag | Conversion in int from hex
-        temp_master_data.append(int(data[1][8], 16))    
-    except:
-        temp_master_data.append(data[1][8])
+    
+    process_id, thread_id = str(data[1][4]).split('.')                  # dataset has process_id.thread_id as float
+    temp_master_data.append(int(process_id))                            # process id
+    temp_master_data.append(int(thread_id))                             # thread id
+
+    temp_master_data.append(int((data[1][5])))                          # parent id
     
     try:
-        data[1][9] = str(data[1][9]).split(' ')[0]      # device object | Conversion in int from hex
-        temp_master_data.append(int(data[1][9], 16))    
+        temp_master_data.append(data[1][6].strip())                     # process name
     except:
-        temp_master_data.append(data[1][9])
+        temp_master_data.append(data[1][6])                             # process name; probably will execute for nan
     
     try:
-        data[1][10] = str(data[1][10]).split(' ')[0]      # File object | Conversion in int from hex
+        temp_master_data.append(data[1][7].strip())                     # major operation type
+    except:
+        temp_master_data.append(data[1][7])                             # major operation type; probably will execute for nan
+    
+    try:
+        temp_master_data.append(data[1][8].strip())                     # minor operation type
+    except:
+        temp_master_data.append(data[1][8])                             # minor operation type; probably will execute for nan
+    
+    try:
+        '''
+            Definition of IRP Flag:
+            fprintf( File, "\t0x%08lx ", RecordData->IrpFlags );
+            fprintf( File, "%s", (RecordData->IrpFlags & IRP_NOCACHE) ? "N":"-" );
+            fprintf( File, "%s", (RecordData->IrpFlags & IRP_PAGING_IO) ? "P":"-" );
+            fprintf( File, "%s", (RecordData->IrpFlags & IRP_SYNCHRONOUS_API) ? "S":"-" );
+            fprintf( File, "%s", (RecordData->IrpFlags & IRP_SYNCHRONOUS_PAGING_IO) ? "Y":"-" );
+        '''
+        
+        data[1][9] = data[1][9].strip()
+        irp_flag_hex = str(data[1][9]).split(' ')[0]                      # IRP flag | Conversion to int from hex
+        temp_master_data.append(int(irp_flag_hex, 16))
+        
+        irp_flag_index = str(data[1][9]).split(' ')[1]
+        
+        if (len(irp_flag_index)) == 4:
+            if(irp_flag_index[0] == 'N'):
+                temp_master_data.append(1)
+            else:
+                temp_master_data.append(0)
+                
+            if(irp_flag_index[1] == 'P'):
+                temp_master_data.append(1)
+            else:
+                temp_master_data.append(0)
+                
+            if(irp_flag_index[2] == 'S'):
+                temp_master_data.append(1)
+            else:
+                temp_master_data.append(0)
+                
+            if(irp_flag_index[3] == 'Y'):
+                temp_master_data.append(1)
+            else:
+                temp_master_data.append(0)
+        
+        else:
+            temp_master_data.append(0)
+            temp_master_data.append(0)
+            temp_master_data.append(0)
+            temp_master_data.append(0)
+        
+    except:
+        temp_master_data.append(np.nan)
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+        temp_master_data.append(0)
+    
+    try:
+        data[1][10] = data[1][10].strip()                                   # device object | Conversion to int from hex
         temp_master_data.append(int(data[1][10], 16))    
     except:
-        temp_master_data.append(data[1][10])
+        temp_master_data.append(np.nan)
+    
+    try:
+        data[1][11] = data[1][11].strip()                                   # File object | Conversion to int from hex
+        temp_master_data.append(int(data[1][11], 16))
+    except:
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][11] = str(data[1][11]).split(' ')[0]      # Trnx | Conversion in int from hex
-        temp_master_data.append(int(data[1][11], 16))    
-    except:
-        temp_master_data.append(data[1][11])
-            
-    try:
-        data[1][12] = str(data[1][12]).split(' ')[0]      # Status | Conversion in int from hex
+        data[1][12] = data[1][12].strip()                                   # Trnx | Conversion to int from hex
         temp_master_data.append(int(data[1][12], 16))    
     except:
-        temp_master_data.append(data[1][12])   
+        temp_master_data.append(np.nan)
+            
+    try:
+        '''
+        Definition:
+            fprintf( File, "\t0x%08lx:0x%p", RecordData->Status, (PVOID)RecordData->Information );
+        '''
+        data[1][13] = data[1][13].strip()
+        status = str(data[1][13]).split(':')[0]                             # Status | Conversion to int from hex
+        temp_master_data.append(int(status, 16))
+        information = str(data[1][13]).split(':')[1]                        # Inform | Conversion to int from hex
+        temp_master_data.append(int(information, 16))
         
-    temp_master_data.append(data[1][13])                    # Inform | Conversion in int from hex
+    except:
+        temp_master_data.append(np.nan)
+        temp_master_data.append(np.nan)
+
              
     try:
-        data[1][14] = str(data[1][14]).split(' ')[0]      # Argument 1 | Conversion in int from hex
+        data[1][14] = data[1][14].strip()                                   # Argument 1 | Conversion to int from hex
         temp_master_data.append(int(data[1][14], 16))    
     except:
-        temp_master_data.append(data[1][14])
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][15] = str(data[1][15]).split(' ')[0]      # Argument 2 | Conversion in int from hex
+        data[1][15] = data[1][15].strip()                                   # Argument 2 | Conversion to int from hex
         temp_master_data.append(int(data[1][15], 16))    
     except:
-        temp_master_data.append(data[1][15])
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][16] = str(data[1][16]).split(' ')[0]      # Argument 3 | Conversion in int from hex
+        data[1][16] = data[1][16].strip()                                   # Argument 3 | Conversion to int from hex
         temp_master_data.append(int(data[1][16], 16))    
     except:
-        temp_master_data.append(data[1][16])
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][17] = str(data[1][17]).split(' ')[0]      # Argument 4 | Conversion in int from hex
+        data[1][17] = data[1][17].strip()                                   # Argument 4 | Conversion to int from hex
         temp_master_data.append(int(data[1][17], 16))    
     except:
-        temp_master_data.append(data[1][17])
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][18] = str(data[1][18]).split(' ')[0]      # Argument 5 | Conversion in int from hex
+        data[1][18] = data[1][18].strip()                                   # Argument 5 | Conversion to int from hex
         temp_master_data.append(int(data[1][18], 16))    
     except:
-        temp_master_data.append(data[1][18])
+        temp_master_data.append(np.nan)
         
     try:
-        data[1][19] = str(data[1][19]).split(' ')[0]      # Argument 6 | Conversion in int from hex
+        data[1][19] = data[1][19].strip()                                   # Argument 6 | Conversion to int from hex
         temp_master_data.append(int(data[1][19], 16))    
     except:
-        temp_master_data.append(data[1][19])
+        temp_master_data.append(np.nan)
         
-    temp_master_data.append(data[1][20])                # buffer length
-    temp_master_data.append(data[1][21])                # entropy
-    temp_master_data.append(data[1][22])                # file name
-    temp_master_data.append(family_id)                  # Family ID
-    temp_master_data.append(class_id)                   # Class ID | 0 for benign and 1 for malicious
+    temp_master_data.append(data[1][20])                                    # buffer length
+    temp_master_data.append(data[1][21])                                    # entropy
+    temp_master_data.append(data[1][22])                                    # file name
+    temp_master_data.append(family_id)                                      # Family ID
+    temp_master_data.append(class_id)                                       # Class ID | 0 for benign and 1 for malicious
     
     return temp_master_data
     
 
 # Scanning all the file names
-os.chdir("benign-irp-logs/machine_1")
+os.chdir("Dataset/benign-irp-logs/machine_1")
 all_filenames = [i for i in glob.glob('*')]
 all_filenames = sorted(all_filenames)
 
@@ -114,14 +211,16 @@ class_val = 0    # 0 for benign and 1 for malicious
 family_id = 0    # 0 for benign and >= 1 for malicious
 # The following are the column names to be used for the processed dataset
 column_names =  [
-                    "operation", #categorical / string
+                    "operation_irp", "operation_fsf", "operation_fio", #categorical / string -> int as flags
                     "sequence_number", #hex / string -> int
                     "pre_operation_time", "post_operation_time", #timestamp -> float
                     "operation_elapsed", #float
                     "process_id", "thread_id", "parent_id", #numerical
                     "process_name", #string
-                    "major_operation_type", #categorical / string
-                    "irp_flag", "device_object", "file_object", "transaction", "status", "inform", #hex / string -> int
+                    "major_operation_type", "minor_operation_type", #categorical / string
+                    "irp_flag", #hex / string -> int
+                    "irp_nocache", "irp_paging_io", "irp_synchoronous_api", "irp_synchoronous_paging_io", #flag values
+                    "device_object", "file_object", "transaction", "status", "inform", #hex / string -> int
                     "arg1", "arg2", "arg3", "arg4", "arg5", "arg6", # hex / string -> int
                     "buffer_length", "entropy", #numerical
                     "file_name", #string
@@ -132,7 +231,7 @@ column_names =  [
 for file_name in all_filenames:
     # Dataframe for processed dataset
     master_data = pd.DataFrame(columns=column_names)
-
+    
     # Importing the dataset
     dataset = pd.read_csv(file_name, sep = '\t')
     dataset = dataset.rename(columns={x:y for x,y in zip(dataset.columns,range(0,len(dataset.columns)))})    # Remaining column names w/ numerics
